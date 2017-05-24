01f3c4f3a9850896b14453a2d6f8d4beef1ca0e6
metalDS <judethomas0403@gmail.com>
2017-05-24 14:17:02 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,20 @@
 */
 #include <iostream>
 #include <string>
+#include <sstream>
+
 #include "player.h"
+#include "control.h"
 
 //enumeration class for specifying actions in parser
 enum class action: bool
 {
+	Invalid,
 	AddItem,
 	RemoveItem,
 	Move,
 	HelpRequest,
+	BadRequest,
 	Error
 };
 
@@ -24,7 +29,10 @@ using FText = std::string;
 
 void introduction();
 void playGame();
-void playerAction(FString input);
+//this function parses player input then performs an action based on that input
+void playerAction(action intent, FString input);
+//parses player input and returns an action enumeration
+action interpretAction(FString input);
 
 FText getPlayerCommand();
 void printInventory();
@@ -35,13 +43,14 @@ player myPlayer;
 int main()
 {
 	//introduce the player to the game
-	//loop while player wants to play the game
-	while (true) //TODO add loop exit functionality, make do-while
+	//outermost loop, when gameOver will ask the player to play again
+	do //TODO add loop exit functionality, make do-while
 	{
 		//TODO ask for players name
 		introduction();
 		playGame();
-	}
+
+	} while (true);
 	
 	//ask if play wants to start again
 }
@@ -60,22 +69,101 @@ void playGame()
 	//secondary event loop
 
 	FText input = "";
-	for(int x = 0; x < 5; x++)
+	do
 	{
-		//describe surroundings to player
-		//take player input
-		input = getPlayerCommand();
-		
+
+		action myAction = action::Invalid;
+		do
+		{
+
+			//describe surroundings to player
+			//take player input
+			input = getPlayerCommand();
+
+			//buffer used as temporary storage to hold the command and target
+			FString buffer;
+			//parse input, return action enum
+			std::stringstream ss;
+			ss << input;
+			ss >> buffer;
+
+			myAction = interpretAction(buffer);
+
+			if (myAction != action::BadRequest)
+			{
+				ss >> buffer;
+				//TODO validate target
+				//std::cout << buffer << std::endl;
+				playerAction(myAction, buffer);
+			}
+			else
+			{
+				std::cout << "im sorry, i didn't understand you.\nenter \"help\" for details.\n";
+			}
+
+		} while (myAction == action::BadRequest); //loops through until a valid action is inputted
+
 		//describe action effect
-		//TODO proccess player input
-		playerAction(input);
-	}
+		printInventory();
+		
+
+	} while (true);
+
 }
 
-void playerAction(FString input)
+void playerAction(action intent, FString input)
 {
-	//parse input, return action enum
-	//do action based on parse
+	//TODO add error message if input targer was not valid
+	switch (intent)
+	{
+	case action::Move:
+		//TODO implement move command
+		break;
+	case action::HelpRequest:
+		//TODO implement help text function
+		break;
+	case action::AddItem:
+		//TODO provide interaction with map objects and player object
+		myPlayer.addValidItem(input);
+		break;
+		//remember to add break statement -.-
+	case action::RemoveItem:
+		myPlayer.removeItem(input);
+		
+		break;
+		
+
+	}
+	
+}
+
+//takes an input and returns an action enum based on what is contained in enum
+action interpretAction(FString input)
+{
+	if (input == "move")
+	{
+		return action::Move;
+	}
+	else if (input == "pickup")
+	{
+		return action::AddItem;
+	}
+	else if (input == "drop")
+	{
+		return action::RemoveItem;
+	}
+	else if (input == "help")
+	{
+		return action::HelpRequest;
+	}
+	else
+	{
+		return action::BadRequest;
+	}
+
+	return action::Error; //incase things get really fucked up
+
+	
 }
 
 FText getPlayerCommand() //TODO validate player command
@@ -87,14 +175,22 @@ FText getPlayerCommand() //TODO validate player command
 	return input;
 }
 
+//TODO fox
 void printInventory()
 {
-	//TVector<FString> inv = myPlayer.getInventory();
-
-	std::cout << "your inventory.\n";
-
-	for (auto item : myPlayer.getInventory())
+	TVector<FString> inv = myPlayer.getInventory();
+	
+	if (!inv.empty())
 	{
-		std::cout << item << std::endl;
+		std::cout << "your inventory.\n";
+		for (auto item : inv)
+		{
+			std::cout << item << std::endl;
+		}
 	}
+	else
+	{
+		std::cout << "your inventory is empty!\n";
+	}
+	
 }
